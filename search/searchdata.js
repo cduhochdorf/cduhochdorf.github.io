b79d620530160e4cd5cb4939e8129f63181e9f53
cduhochdorf <cduhochdorf@users.noreply.github.com>
2024-06-28 20:28:03 +0000

    deploy: d068263be45ffba28ab6c9c5791149fa5c8cdc36

---
+++ search/searchdata.js
@@ -0,0 +1,21 @@
+var indexSectionsWithContent =
+{
+  0: "adirstwz§✉",
+  1: "rz",
+  2: "diw✉"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "files",
+  2: "pages"
+};
+
+var indexSectionLabels =
+{
+  0: "All",
+  1: "Files",
+  2: "Pages"
+};
+
